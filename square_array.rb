0bd733b06623dbe236a894c34ed12435d70eb7dd
Andrew Eames <ajerxs@gmail.com>
2020-11-04 23:53:39 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,14 @@
 def square_array(array)
+<<<<<<< HEAD
   new_numbers =[]
   array.each do |numbers|
     new_numbers << numbers**2
   end
   new_numbers
+=======
+  array.each do |numbers|
+    numbers**2
+  end
+  square_array(array)
+>>>>>>> 473d1dca89f408b3dddf7c2ef0fc7f07c6e3aed1
 end
